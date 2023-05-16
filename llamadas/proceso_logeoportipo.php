42c666a5baccc56b3041848eac61c770dbc1e2d3
CristhianV15 <cmvc99@gmail.com>
2023-05-16 00:28:43 -0500

    Funciona Logeo (Usuario)

--- llamadas/proceso_logeoportipo.php
+++ llamadas/proceso_logeoportipo.php
@@ -41,7 +41,7 @@
             } else{
                 $_SESSION['usuario'] = $usu;
                 $_SESSION['email'] = $email;
-                header("location:../paginas/paginaPrincipalCliente.php");}
+                header("location:../pages/Cliente/cliente.php");}
             break;
             
 
