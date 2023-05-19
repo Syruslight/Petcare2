a5d1ee7c57823a574768f0a6aec56667dd3592a3
unknown <estefanyromanbroncano@gmail.com>
2023-05-19 18:19:45 -0500

    previsualizar img, te gané luis

--- js/registroNewUs.js
+++ js/registroNewUs.js
@@ -75,3 +75,19 @@ prevBtnFourth.addEventListener("click", function (event) {
   progressText[current - 2].classList.remove("active");
   current -= 1;
 });
+
+//previsualizar imagen
+const defaulfile = "https://lh5.googleusercontent.com/proxy/9vqIPeIeHQHyGEo43DlSgD-DUtidieclv56O6UoAcYNGPXGNnZwFJL2V7oSodehCB1YT28jit7pMSVjNTnrBOnlBxW0CiRmOeH22FlPockzEbfdQPHLkDMPcgMwWdNfVHF1r2QpUk6W_aY_J87A9lFtYKMHf8_xhkMB7l_4=s0-d";
+const img =document.getElementById('img');
+const file = document.getElementById('foto');
+file.addEventListener('change', e=>{
+if(e.target.files[0]){
+  const reader= new FileReader();
+  reader.onload = function(e){
+    img.src= e.target.result;
+  }
+  reader.readAsDataURL(e.target.files[0])
+}else{
+  img.src=defaulfile;
+}
+});
