75efa5f40cc662e65d0d66d168f415a057f1c058
unknown <estefanyromanbroncano@gmail.com>
2023-05-19 02:56:28 -0500

    vacunas y lotes sin front, falta poner back

---
+++ pages/Veterinario/vacunas/moduloVacunaLote.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <h2 class="titulo_form">Registro de Lote</h2>
+        <div class="grupotex">
+            <label for="" class="etiqueta_nombre">Lote:</label>
+            <input type="text" name="" id="" class="ingreso_datos">
+
+        </div>
+        <div class="grupotex">
+            <label for="" class="etiqueta_nombre">Tipo:</label>
+            <input type="texto" name="" id="" class="ingreso_datos">
+        </div>
+        <div class="grupotex">
+            <label for="" class="etiqueta_nombre">Descripción:</label>
+            <input type="text" name="" id="" class="ingreso_datos">
+        </div>
+        <button type="submit" class="btn_envio">Agregar</button>
+
+    </form>
+
+
+    <form action="" method="post">
+        <h2 class="titulo_detallevac">Formulario vacuna</h2>
+        <P class="textobusqueda">Ingrese el RENIAN de la mascota:</P>
+        <!--Se filtra por  LA  MASCOTA - RENINAN-->
+        <input type="search" name="" id="" class="busqueda" required>
+        <input type="text" placeholder="id_lote"><input type="text" placeholder="iDmascota">
+        <div class="grupotex">
+            <label for="" class="etiquenom">Nombre de la mascota</label>
+            <input type="text" name="" id="" class="etiquelec">
+
+        </div>
+        <div class="grupotex">
+            <label for="" class="etiqueta_nombre">Lote:</label>
+            <input type="text" name="" id="" class="ingreso_datos">
+        </div>
+        <div class="grupotex">
+            <label for="" class="etiqueta_nombre">Tipo:</label>
+            <input type="texto" name="" id="" class="ingreso_datos">
+        </div>
+        <div class="grupotex">
+
+            <label for="" class="etiqueta_nombre">Proxima fecha:</label>
+            <input type="date" name="" id="" class="ingreso_datos">
+        </div>
+        <div class="grupotex">
+            <label for="" class="etiqueta_nombre">Observación</label>
+            <input type="text" name="" id="" class="ingreso_datos">
+        </div>
+        <label for="" class="etiqueta_nombre">Restricciones</label>
+        <input type="text" name="" id="" class="ingreso_datos">
+
+    </form>
+</body>
+
+</html>
