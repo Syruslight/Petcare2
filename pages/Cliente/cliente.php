74dee2927a72a2410ed8c39ad680fa08396da808
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-06-16 16:22:25 -0500

    enrutamiento correcto

--- pages/Cliente/cliente.php
+++ pages/Cliente/cliente.php
@@ -337,8 +337,8 @@ $mascotas = listarDatosMascotaDasboardCliente($idCliente, $conn);
     </section>
 
     <script src="../../js/modalMascota.js"></script>
-    <script src="../../js/Modal.js"></script>
-    <script src="../../js/registroNewUs.js"></script>
+    <script src="../../js/Interacciones.js"></script>
+    <script src="../../js/previsualizarImagen.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
--- pages/Mascota/mascotaIndex.php
+++ pages/Mascota/mascotaIndex.php
@@ -215,7 +215,7 @@ foreach (listarCliente($email, $conn) as $key => $value) {
         include '../Cliente/editarCliente/modalEditarCliente.php';
         ?>
 
-        <script src="../../js/registroNewUs.js"></script>
+        <script src="../../js/previsualizarImagen.js"></script>
         <script src="../../js/Interacciones.js"></script>
         <script src="../../js/modalMascota.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
