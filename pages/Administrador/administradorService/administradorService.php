1207b1c262db2146424831f98a7797bc65e3848d
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-06-17 04:37:17 -0500

    mejorado el crear cuentas vet (administrador) no usar el editar foto de veterinario, no funciona

--- pages/Administrador/administradorService/administradorService.php
+++ pages/Administrador/administradorService/administradorService.php
@@ -17,11 +17,7 @@ $conn = conectar();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href='administradorService.css'>
-<<<<<<< HEAD
     <link rel="stylesheet" href='../editAdministrador/editModalAdministrador.css'>
-=======
-    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
->>>>>>> 31f2aab4fcef573c21a4f168215d9875357f67fe
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Pagina de administrador</title>
 </head>
