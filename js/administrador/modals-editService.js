b58976af2d0101f25e4e0d7af25f94c433de5ab0
CristhianV15 <cmvc99@gmail.com>
2023-06-27 15:29:42 -0500

    Se soluciono el bug del modal de editar
    Ya funciona en todos los registros, se quito el toggle item momentáneamente, falta poner centrada la foto al listar y en el modal de editar @Manuelrazuri (arreglar front para poner el toggle )

--- js/administrador/modals-editService.js
+++ js/administrador/modals-editService.js
@@ -1,15 +1,35 @@
 function openModalEdithService() {
-  const openModal = document.querySelector(".openModalEdithService");
-  const modal = document.querySelector(".modal-edithService");
-  const closeModal = document.querySelector("#close1");
-  openModal.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.classList.add("modalService--show");
+  const openModalButtons = document.querySelectorAll('.openModalEdithService');
+  const modal = document.querySelector('.modal-edithService');
+  const closeModal = document.querySelector('#close1');
+  
+  openModalButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      const nombreServicio = e.target.getAttribute("data-nombre");
+      const precioServicio = e.target.getAttribute("data-precio");
+      const descripcionServicio = e.target.getAttribute("data-descripcion");
+      const fotoperfil = e.target.getAttribute("data-foto");
+      const fotoperfil1 = fotoperfil; // Nueva variable foto por defecto (la que esta guardada en la BD)
+      // aqui falta otra variable para guardar la nueva foto en caso de haber
+
+      document.querySelector("#nombreServicioEnvio").value = nombreServicio;
+      document.querySelector("#precioServicioEnvio").value = precioServicio;
+      document.querySelector("#descripcionServicioEnvio").value = descripcionServicio;
+      document.querySelector("#perfil-img").src = "../../../imagenes/productos_servicios/servicios/" + fotoperfil1;
+      
+      
+      
+      
+      modal.classList.add('modalService--show');
+    });
   });
-  closeModal.addEventListener("click", (e) => {
+  
+  closeModal.addEventListener('click', (e) => {
     e.preventDefault();
-    modal.classList.remove("modalService--show");
+    modal.classList.remove('modalService--show');
   });
+  
 }
 openModalEdithService();
 
