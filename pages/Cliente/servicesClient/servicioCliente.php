453be4b7bc7854bda2f65717a46877a6ab0c4780
Manuel Razuri <1412938@utp.edu.pe>
2023-07-08 02:27:57 -0500

    front-services

--- pages/Cliente/servicesClient/servicioCliente.php
+++ pages/Cliente/servicesClient/servicioCliente.php
@@ -45,10 +45,133 @@ foreach (listarCliente($email, $conn) as $key => $value) {
         <div class="wrapper-service">
             <div class="header-table">
                 <div class="search-service">
-
+                <img class="image-search" src="../../../imagenes/perfilCliente/search.svg" />
+                <input class="input-search" type="search" name="buscarServicio" placeholder="" id="busqueda">
+                </div>
+                <span class="spancitoo"> Especie:</span>
+                <div class="select-service">
+                <select name="select">
+                <option value="value1">Perros (48)</option>
+                <option value="value2" selected>Gatos (48)</option>
+                <option value="value3">Conejos (48)</option>
+                </select>
                 </div>
             </div>
-            <div></div>
+            <div class="wrapper-cardService">
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+                <div class="cards-service">
+                    <div class="image-card">
+                    <img class="card-imageService" src="../../../imagenes/perfilCliente/serviceDog.png" />
+                    </div>
+                    <div class="info-card">
+                        <h1 class="title-cardService">Servicio de Baño</h1>
+                        <div class="price-time">
+                        <span class="price">Precio: <span class="duration">s/45.00</span></span>
+                        <span class="price">Duracion: <span class="duration">45 min</span> </span>
+                        </div>
+                        <div class="after-text">
+                            <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
+                        </div>
+                        <button class="reserve-service">Reservar</button>
+                    </div>
+                </div>
+           
+            </div>
         </div>
     </div>
 </div>
