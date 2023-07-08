e62cb6005c722a345ec356f8f6e097ba43e72ea9
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-07-08 02:52:40 -0500

    multipasos dentro de modal

--- pages/Cliente/servicesClient/servicioCliente.php
+++ pages/Cliente/servicesClient/servicioCliente.php
@@ -71,7 +71,7 @@ foreach (listarCliente($email, $conn) as $key => $value) {
                         <div class="after-text">
                             <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
                         </div>
-                        <button class="reserve-service">Reservar</button>
+                        <button onclick="openModalReservarServicio()" class="reserve-service">Reservar</button>
                     </div>
                 </div>
                 <div class="cards-service">
@@ -87,7 +87,7 @@ foreach (listarCliente($email, $conn) as $key => $value) {
                         <div class="after-text">
                             <span class="text-cardService">Este servicio se basa en el baño usando shampoo, acondiconardor, crema para un mejor cuidado del pelaje canino</span>
                         </div>
-                        <button class="reserve-service">Reservar</button>
+                        <button onclick="openModalReservarServicio()" class="reserve-service">Reservar</button>
                     </div>
                 </div>
                 <div class="cards-service">
@@ -175,7 +175,116 @@ foreach (listarCliente($email, $conn) as $key => $value) {
         </div>
     </div>
 </div>
-  
+
+
+
+
+
+
+
+
+
+<section class="modalReservar">
+                <div class="modalReservar__container">
+                    <div class="cuadro_modalReservar">
+                        <div class="top-formReservar">
+                            <div class="tituloReservar">
+                                <h1>Reservar cita</h1>
+                            </div>
+                            <div id="CloseModalReservar">
+                                &#10006
+                            </div>
+                        </div>
+                        <div class="container">
+   
+    <div class="progress-bar">
+      <div class="step">
+        <p>Paso 1</p>
+        <div class="bullet">
+          <span>1</span>
+        </div>
+        <div class="check fas fa-check"></div>
+      </div>
+      <div class="step">
+        <p>Paso 2</p>
+        <div class="bullet">
+          <span>2</span>
+        </div>
+        <div class="check fas fa-check"></div>
+      </div>
+      <div class="step">
+        <p>Paso 3</p>
+        <div class="bullet">
+          <span>3</span>
+        </div>
+        <div class="check fas fa-check"></div>
+      </div>
+      
+    </div>
+    <div class="form-outer">
+      <!--Formulario para registrar usuario nuevo-->
+      <form  method="post" enctype="multipart/form-data">
+        <!--Se realizan 4 divisiones-->
+        <div class="page slide-page">
+         
+          <div class="field">
+            <button class="firstNext next">Siguiente</button>
+          </div>
+        </div>
+        <div class="page">
+          
+          
+          <div class="field btns">
+            <button class="prev-1 prev">Atrás</button>
+            <button class="next-1 next">Siguiente</button>
+          </div>
+        </div>
+        <div class="page">
+         
+          <div class="field btns">
+            <button class="prev-2 prev">Atrás</button>
+            <button class="submit">Enviar</button>
+          </div>
+        </div>
+        
+      </form>
+    </div>
+  </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script>
+  function openModalReservarServicio(){
+  const openModal = document.querySelector(".reserve-service");
+  const modal = document.querySelector(".modalReservar");
+  const closeModal = document.querySelector("#CloseModalReservar");
+  openModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("modalReservar--show");
+  });
+  closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.remove("modalReservar--show");
+  });
+}
+</script>
+
+<script src="../../../js/reservarCItaMulti.js"></script>
 </body>
 
 </html>
